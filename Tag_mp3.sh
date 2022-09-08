7a37c6e003a9cb40c5738e7349f8cd8db55e21b1
The Audit <mackaywillrpi@gmail.com>
2022-09-08 20:27:46 +0100

    added track number increment, option to remove all tags and check for COVER.jpg

--- Tag_mp3.sh
+++ Tag_mp3.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+function yesno() {
+  answer=$(whiptail --yesno "$1" 0 0 3>&1 1>&2 2>&3; echo $?)
+  if [ $answer -eq 0 ]; then
+    eyeD3 --remove-all *.mp3 > /dev/null
+  fi
+}
+
+yesno "Do you want to reset all tags on these files?"
+
 Title=$(whiptail --title "Album Input Box" --inputbox "What is the album title?" 10 60 - 3>&1 1>&2 2>&3)
 
 exitstatus=$?
@@ -36,37 +46,47 @@ else
     echo "You chose Cancel."
 fi
 
+tn=1
+
 for file in *.mp3; 
 do echo $file;
-t=1
-Track=$(whiptail --title "Track Input Box" --inputbox "What is the Track Title?" 10 60 "$file" 3>&1 1>&2 2>&3)
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    eyeD3 -t "$Track" "$file" > /dev/null
-else
-    echo "You chose Cancel."
-fi
 
-TrackN=$(whiptail --title "Track Input Box" --inputbox "What is the Track Number?" 10 60 $t 3>&1 1>&2 2>&3)
-exitstatus=$?
-if [ $exitstatus = 0 ]; then
-    eyeD3 -n "$TrackN" "$file" > /dev/null
-else
-    echo "You chose Cancel."
-fi
+    
 
-t=$t + 1
+    Track=$(whiptail --title "Track Input Box" --inputbox "What is the Track Title?" 10 60 "$file" 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        eyeD3 -t "$Track" "$file" > /dev/null
+    else
+        echo "You chose Cancel."
+    fi
+
+    TrackN=$(whiptail --title "Track Input Box" --inputbox "What is the Track Number?" 10 60 $tn 3>&1 1>&2 2>&3)
+    exitstatus=$?
+    if [ $exitstatus = 0 ]; then
+        eyeD3 -n "$TrackN" "$file" > /dev/null
+        tn=$((TrackN+1))
+    else
+        echo "You chose Cancel."
+    fi
+
+    
 
 done
 
+FILE=COVER.jpg
+if [ -f "$FILE" ]; then
+    {
+        for ((i = 0 ; i <= 100 ; i+=20)); do
+            if [ $i = 0 ]; then
+                eyeD3 --add-image "COVER.jpg:FRONT_COVER" *.mp3
+            fi
+            sleep 0.2
+            echo $i
+        done
+    } | whiptail --gauge "Please wait while updating cover art." 6 60 0
+else
+    whiptail --msgbox "No cover art to update, if you want to add cover art you will need to an appropriate image file named COVER.jpg in this directory" 10 50
+fi
 
-#!/bin/bash
-{
-    for ((i = 0 ; i <= 100 ; i+=20)); do
-        if [ $i = 0 ]; then
-            eyeD3 --add-image "COVER.jpg:FRONT_COVER" *.mp3
-        fi
-        sleep 0.2
-        echo $i
-    done
-} | whiptail --gauge "Please wait while updating cover art." 6 60 0
+whiptail --msgbox "Completed tagging" 10 50
