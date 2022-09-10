a9c024d61935ddf4fd421cb8bae07b239cbf53a8
The Audit <mackaywillrpi@gmail.com>
2022-09-10 21:24:10 +0100

    9571 Youtube Downloader Wrapper

---
+++ YouTubeDownload.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+YouTubeURL=$(whiptail --title "Artist Input Box" --inputbox "What YouTube URL are you taking music from?" 10 60 - 3>&1 1>&2 2>&3)
+
+youtube-dl -f "bestaudio/best" -ciw -o "%(title)s.%(ext)s" -v --extract-audio --audio-quality 0 --audio-format mp3 $YouTubeURL
